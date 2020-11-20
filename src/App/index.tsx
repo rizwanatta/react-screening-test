f1752d69d53c64e3be239a5783ac06ff957a9b83
rizwan atta <rizwanatta@protonmail.com>
2020-11-20 16:20:46 +0500

    feat(google font): added google font roboto

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styled from 'styled-components';
 import "./App.css";
 
+
+const MyButton = styled.button`
+background: transparent;
+ border-radius: 3px;
+ border: 2px solid palevioletred;
+ color: palevioletred;
+ margin: 0 1em;
+ padding: 0.25em 1em;
+`;
 function App() {
-  return <div>Hello World</div>;
+  return <div>
+    Hello World
+    <MyButton> TEST BUTTON IS HERE</MyButton>
+  </div>;
 }
 
 export default App;
